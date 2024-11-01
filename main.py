e59d4b4eac168818e4470e27ca9ae257a7e54077
andrkos <110351109+andrkos@users.noreply.github.com>
2024-11-01 23:47:39 +0200

    time delay added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import time
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -7,7 +9,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Salam, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+time.sleep(5)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
